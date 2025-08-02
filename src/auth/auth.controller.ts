055364c80138c4b655f356bcdefab09cd1c9cb68
yoel ider <yoeli5746@gmail.com>
2025-08-03 02:08:58 +0300

    finish all guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,20 +16,5 @@ export class AuthController {
   Signing(@Req() req: Request) {
     const token = req.headers['authorization']
     return this.authService.Signing(token);
-  }
-
-  @Get(':id')
-  findOne(@Param('username') username: string) {
-    return this.authService.findOne(username);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
   }
 }
--- src/shift/shift.controller.ts
+++ src/shift/shift.controller.ts
@@ -1,32 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { ShiftService } from './shift.service';
 import { CreateShiftDto } from './dto/create-shift.dto';
-import { UpdateShiftDto } from './dto/update-shift.dto';
+import { JwtAuthGuard } from 'src/common/guards/roles/jwt.guard';
+import { RolesGuard } from 'src/common/guards/roles/roles.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('shift')
 export class ShiftController {
-  constructor(private readonly shiftService: ShiftService) {}
+  constructor(private readonly shiftService: ShiftService) { }
 
+  @UseGuards(RolesGuard)
   @Post('creat-shift')
   create(@Body() createShiftDto: CreateShiftDto) {
     return this.shiftService.create(createShiftDto);
   }
 
+  @UseGuards(RolesGuard)
   @Get('get-all')
   findAll() {
     return this.shiftService.findAll();
   }
 
-  @Get('get-shift/:id')
-  async findOne(@Param('id') id: string) {
-    return await this.shiftService.findOne(+id);
+  @Get('get-my-shifts')
+  async findOne(@Req() req: Request) {
+    const user = (req as any).user;
+    const userId = user.sub;
+    return await this.shiftService.findUserShifts(userId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateShiftDto: UpdateShiftDto) {
-    return this.shiftService.update(+id, updateShiftDto);
-  }
-
+  @UseGuards(RolesGuard)
   @Delete('delete/:id')
   async remove(@Param('id') id: string) {
     return await this.shiftService.remove(+id);
